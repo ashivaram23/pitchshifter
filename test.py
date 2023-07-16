706dcee4f9d0d11951784402bd6bb6f84f1cd533
Amogh Shivaram <ashivaram@wisc.edu>
2023-07-15 22:39:04 -0500

    continue writing overlap-add

--- test.py
+++ test.py
@@ -4,43 +4,50 @@ import scipy.io.wavfile
 import sys
 
 
-# DISGUSTING CODE EVERYWHERE but works (basic OLA with no/square window function) with artifacts and not perfectly ?? need SOMETHING about the padding
-# both for the before(?) and after due to the nature of the window overlap stuff in first place (and correspondingly for the input otherwise it wont work?), but also for the separate end round-up thing which is currently messily bandaged
-# and those two might affect each other ofc, and then have to retrim properly at end
-# zero pad input and output by respective offset sizes before start. then, on TOP of that, for ease first round up the length (length INCLUDING the start padding in the calculation, so I guess that would also have to have been included in the num frames before too) to frame count ceiling result, and then final question is does there need to be addditional (possibly overlapping) padding at the end?
-
-# also cant min the window max amp at 1 because still need to compensate if less, just do normally and then clip to ensure all = 1 no rounding mess
-# end is pad offset first then round combined value up?
+# Working overlap add, with fluttering as expected. Now just have to implement resampling (with
+# what interpolation?, etc), calculate the right time stretch + resample combination for a given 
+# pitch, and then implement the WAVEFORM SIMILARITY part for no fluttering
+# And make everything neat, efficient, etc using the right variables (constants/modifiable globals
+# for rate and channels, make frame len and offset parameters, etc)
+# Then do it in eg C++, translating numpy funcs to loops
 def time_stretch(input, multiplier):
-    frame_size = 100
-    out_offset = 70
+    frame_len = int(44100 * 100 / 1000)
+    out_offset = int(44100 * 70 / 1000)
 
-    output = np.zeros(int(np.ceil(len(input) * multiplier)), dtype="float32")
-    input_ms = 1000 * len(input) / 44100
-    output_ms = 1000 * len(output) / 44100
+    in_len = len(input)
+    out_len = int(len(input) * multiplier)
 
-    num_frames = int(np.ceil((output_ms - (frame_size - out_offset)) / out_offset))
-    in_offset = (input_ms - frame_size) / (num_frames - 1)
+    in_padded_len = in_len + 2 * out_offset
+    out_padded_len = out_len + 2 * out_offset
 
-    output_max_amp = np.zeros(len(output), dtype="float32")
-    window = np.ones(int(44100 * frame_size / 1000), dtype="float32")
+    num_frames = 1 + (out_padded_len - frame_len) / out_offset
+    in_offset = int((in_padded_len - frame_len) / (num_frames - 1))
 
-    for i in range(num_frames - 1):
-        out_start = int(44100 * (out_offset * i) / 1000)
-        out_end = out_start + int(44100 * (frame_size) / 1000)
+    in_padded = np.zeros(int(in_offset * (np.ceil(num_frames) - 1) + frame_len), dtype="float32")
+    in_padded[out_offset : out_offset + in_len] = input
+    out_padded = np.zeros(int(out_offset * (np.ceil(num_frames) - 1) + frame_len), dtype="float32")
+    out_max_amp = out_padded.copy()
 
-        in_start = int(44100 * (in_offset * i) / 1000)
-        in_end = in_start + int(44100 * (frame_size) / 1000)
+    window = np.zeros(frame_len)
+    window[0 : int(frame_len / 2)] = np.linspace(0, 1, int(frame_len / 2))
+    window[int(frame_len / 2) : frame_len] = np.linspace(1, 0, frame_len - int(frame_len / 2))
 
-        output[out_start:out_end] += input[in_start:in_end] * window[:(input[in_start:in_end].size)]
-        output_max_amp[out_start:out_end] += window
+    for i in range(int(np.ceil(num_frames))):
+        in_start = in_offset * i
+        in_end = in_start + frame_len
+        out_start = out_offset * i
+        out_end = out_start + frame_len
 
-    output_max_amp = np.maximum(output_max_amp, 1.0)
-    output /= output_max_amp
-    return output
+        out_padded[out_start:out_end] += in_padded[in_start:in_end] * window
+        out_max_amp[out_start:out_end] += window
+    
+    np.seterr(invalid="ignore")
+    out_padded = np.minimum(out_padded / out_max_amp, 1.0)
+    np.seterr(invalid="warn")
+
+    return out_padded[out_offset : out_offset + out_len]
 
 
-# stereo audio 44.1 kHz, make more general ofc
 _, data = scipy.io.wavfile.read(sys.argv[1])
 
 clip = np.array(data, dtype="float32")
@@ -50,16 +57,32 @@ clip /= max(max_amp, np.max(np.abs(clip)))
 left_channel = clip[:, 0]
 right_channel = clip[:, 1]
 
-stretched_left = left_channel
-stretched_right = right_channel
-stretched_left = time_stretch(left_channel, 1.0)
-stretched_right = time_stretch(right_channel, 1.0)
+old_len = len(left_channel)
+left_channel = time_stretch(left_channel, 1.2)
+right_channel = time_stretch(right_channel, 1.2)
+print(len(left_channel) / old_len)
 
 p = pyaudio.PyAudio()
 stream = p.open(44100, 2, pyaudio.paFloat32, False, True)
-interleaved = np.empty(stretched_left.size * 2, dtype="float32")
-interleaved[0::2] = stretched_left
-interleaved[1::2] = stretched_right
+interleaved = np.empty(left_channel.size * 2, dtype="float32")
+interleaved[0::2] = left_channel
+interleaved[1::2] = right_channel
 stream.write(interleaved.tobytes())
 stream.close()
 p.terminate()
+
+# _, data = scipy.io.wavfile.read(sys.argv[1])
+
+# clip = np.array(data, dtype="float32")
+# max_amp = 1.0 if data.dtype == np.float32 else np.iinfo(data.dtype).max
+# clip /= max(max_amp, np.max(np.abs(clip)))
+
+# old_len = len(clip)
+# clip = time_stretch(clip, 0.5)
+# print(len(clip) / old_len)
+
+# p = pyaudio.PyAudio()
+# stream = p.open(44100, 1, pyaudio.paFloat32, False, True)
+# stream.write(clip.tobytes())
+# stream.close()
+# p.terminate()
