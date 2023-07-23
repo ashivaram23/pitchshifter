5bfcefb70b4525aa545306f1383af4264324a6fb
Amogh Shivaram <ashivaram@wisc.edu>
2023-07-23 00:12:48 -0500

    work on js code

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+const memory = new WebAssembly.Memory({initial: 256, maximum: 32768});
+const memoryFloat32View = new Float32Array(memory.buffer);
+const importObject = {env: {memory: memory, emscripten_notify_memory_growth: growMemory}};
+const wasmSource = WebAssembly.instantiateStreaming(fetch("repitch.wasm"), importObject);
+
+const audioContext = new AudioContext();
+const playback = document.getElementById("playback");
+let originalAudioArrayBuffer;
+
 const fileChooser = document.getElementById("file-select");
 fileChooser.addEventListener("change", openFile);
 document.getElementById("file-button").onclick = (e) => fileChooser.click();
@@ -6,46 +15,34 @@ function openFile(event) {
   const file = fileChooser.files[0];
   const filenameLabel = document.getElementById("filename");
   filenameLabel.innerText = file.name;
-
-  const playback = document.getElementById("playback");
   playback.src = URL.createObjectURL(file);
 
   const reader = new FileReader();
-  reader.onload = setUpAudioContext;
+  reader.onload = (e) => {
+    originalAudioArrayBuffer = e.target.result;
+    audioContext.decodeAudioData(originalAudioArrayBuffer).then((audioBuffer) => processAudioBuffer(audioBuffer));
+  };
   reader.readAsArrayBuffer(file);
+
+  // Maybe deal with allocating the input memory for wasm module here (and only free it with another method when new file opened), so same thing can be accessed
 }
 
-// Dont do this in the async route, do it first and do the async upon each change. fix this later once done checking if it works
-function setUpAudioContext(event) {
-  const audioContext = new AudioContext();
-  audioContext.decodeAudioData(event.target.result).then((val) => sendToWasm(val));
-}
-
-// THIS IS ALL VERY MESSY and only to test that it works, must rewrite with a sensible flow of events after
-function sendToWasm(audioBuffer) {
+async function processAudioBuffer(audioBuffer) {
+  // Do both channels properly and then send back to playback src (or change playback to be custom audiocontext play instead of basic html audio), make downloadable, etc
   const leftChannel = audioBuffer.getChannelData(0);
   const rightChannel = audioBuffer.getChannelData(1);
-  
-  const memory = new WebAssembly.Memory({initial: 256, maximum: 32768});
-  const memoryFloat32View = new Float32Array(memory.buffer);
-  const memoryUint8View = new Uint8Array(memory.buffer);
-  const importObject = {env: {memory: memory, emscripten_notify_memory_growth: growMemory}};
-  WebAssembly.instantiateStreaming(fetch("repitch.wasm"), importObject).then(source => {
+
+  await wasmSource.then(source => {
     let inputPointer = source.instance.exports.allocateInputMemory(leftChannel.length);
     console.log(inputPointer);
     
-    memoryUint8View.set(leftChannel, inputPointer); // should this be memoryfloat32 view but divide input pointer by 4 or how does it work
-    let outputPointer = source.instance.exports.repitchAndStretch(inputPointer, leftChannel.length, 1.5, -5);
+    memoryFloat32View.set(leftChannel, inputPointer / 4);
+    let outputPointer = source.instance.exports.repitchAndStretch(inputPointer, leftChannel.length, timeSlider.value, pitchSlider.value);
     console.log(outputPointer);
-    
-    // Again this nested promises code is very messy and also WONT WORK for updating multiple times, this is just for testing but rewrite to create the required readers and contexts and worklets at the start and set up callbackls for each update as appropriate and properly do all the async/promise/callback etc stuff
+    console.log(new Int32Array(memory.buffer)[outputPointer / 4]);
+    source.instance.exports.freeAllocatedMemory(outputPointer); // Comment this out to cause the memory growth errors in console and fix them independently, since those should be prevented regardless, then add this back in
   });
 }
-
-// Write C function to allocate memory for input given length first and return pointer
-// JS uses that pointer, copies the f32 samples into Wasm memory there, then sends that to repitchAndStretch
-// Then uses the pointer for the output it gets to copy it back into something to play audio
-// (And calls the functions needed to FREE result, and also make the C code free input, maybe in repitchAndStretch itself once done with it)
 
 const pitchSlider = document.getElementById("semitones");
 const pitchLabel = document.getElementById("semitones-label");
@@ -59,20 +56,13 @@ timeSlider.addEventListener("input", (e) => timeLabel.innerText = timeFormat.for
 timeSlider.addEventListener("change", updateAudio);
 
 function updateAudio() {
-  console.log(`${pitchSlider.value}, ${timeSlider.value}`);
-}
-
-const memory = new WebAssembly.Memory({initial: 256, maximum: 32768});
-const memoryFloat32View = new Float32Array(memory.buffer);
-const memoryUint8View = new Uint8Array(memory.buffer);
-const importObject = {env: {memory: memory, emscripten_notify_memory_growth: growMemory}};
-WebAssembly.instantiateStreaming(fetch("repitch.wasm"), importObject).then(useWasm);
-
-function useWasm(source) {
-  // const exports = source.instance.exports;
-  // console.log(exports.repitchAndStretch());
+  if (originalAudioArrayBuffer != undefined) {
+    audioContext.decodeAudioData(originalAudioArrayBuffer).then((audioBuffer) => processAudioBuffer(audioBuffer));
+    console.log(`${pitchSlider.value}, ${timeSlider.value}`);
+  }
 }
 
 function growMemory() {
+  memoryFloat32View = new Float32Array(memory.buffer);
   // Handle memory growth
 }
--- repitch.c
+++ repitch.c
@@ -1,12 +1,11 @@
 #include <math.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 // Stores the processed audio output to return to the user
 struct result {
+  int resultLength;
   float *resultData;
-  long resultLength;
 };
 
 /*
@@ -17,14 +16,14 @@ struct result {
  * output -- pointer to store output array
  * speed -- multiplier for the audio's speed
  */
-long resample(float *input, long inputLength, float **output, float speed) {
-  long outputLength = inputLength / speed;
+int resample(float *input, int inputLength, float **output, float speed) {
+  int outputLength = inputLength / speed;
   *output = calloc(outputLength, sizeof(float));
 
-  for (long i = 0; i < outputLength; i++) {
+  for (int i = 0; i < outputLength; i++) {
     float correspondingIndex = i * (inputLength / (float)outputLength);
-    long lowIndex = floor(correspondingIndex);
-    long highIndex = ceil(correspondingIndex);
+    int lowIndex = floor(correspondingIndex);
+    int highIndex = ceil(correspondingIndex);
     float interpolate = correspondingIndex - lowIndex;
     (*output)[i] = input[lowIndex] * (1 - interpolate) + input[highIndex] * interpolate;
   }
@@ -38,8 +37,8 @@ long resample(float *input, long inputLength, float **output, float speed) {
  * window -- array for the window function values
  * length -- length of window function in samples
  */
-void fillWindowFunction(float *window, long length) {
-  for (long i = 0; i < length; i++) {
+void fillWindowFunction(float *window, int length) {
+  for (int i = 0; i < length; i++) {
     float xValue = i * (2 * M_PI / (float)length);
     window[i] = 0.5 * (1 - cos(xValue));
   }
@@ -52,7 +51,7 @@ void fillWindowFunction(float *window, long length) {
  * b -- destination array
  * length -- number of items to copy
  */
-void copyArray(float *a, float *b, long length) {
+void copyArray(float *a, float *b, int length) {
   memcpy(b, a, length * sizeof(float));
 }
 
@@ -64,8 +63,8 @@ void copyArray(float *a, float *b, long length) {
  * c -- destination array
  * length -- lengths of arrays being multiplied
  */
-void multiplyArrays(float *a, float *b, float *c, long length) {
-  for (long i = 0; i < length; i++) {
+void multiplyArrays(float *a, float *b, float *c, int length) {
+  for (int i = 0; i < length; i++) {
     c[i] = a[i] * b[i];
   }
 }
@@ -78,8 +77,8 @@ void multiplyArrays(float *a, float *b, float *c, long length) {
  * c -- destination array
  * length -- lengths of arrays being added
  */
-void addArrays(float *a, float *b, float *c, long length) {
-  for (long i = 0; i < length; i++) {
+void addArrays(float *a, float *b, float *c, int length) {
+  for (int i = 0; i < length; i++) {
     c[i] = a[i] + b[i];
   }
 }
@@ -91,8 +90,8 @@ void addArrays(float *a, float *b, float *c, long length) {
  * x -- float scalar to add
  * length -- length of array
  */
-void addScalarToArray(float *a, float x, long length) {
-  for (long i = 0; i < length; i++) {
+void addScalarToArray(float *a, float x, int length) {
+  for (int i = 0; i < length; i++) {
     a[i] += x;
   }
 }
@@ -104,9 +103,9 @@ void addScalarToArray(float *a, float x, long length) {
  * b -- the other array
  * length -- lengths of arrays
  */
-float dotProduct(float *a, float *b, long length) {
+float dotProduct(float *a, float *b, int length) {
   float sum = 0;
-  for (long i = 0; i < length; i += 8) {
+  for (int i = 0; i < length; i += 8) {
     sum += a[i] * b[i];
   }
 
@@ -122,22 +121,22 @@ float dotProduct(float *a, float *b, long length) {
  * output -- pointer to store output array
  * multiplier -- multiplier for the audio's duration
  */
-long timeStretch(float *input, long inputLength, float **output, float multiplier) {
+int timeStretch(float *input, int inputLength, float **output, float multiplier) {
   // Sets size of the segments to split the audio into and the offset between overlapping segments in the output
-  long segmentLength = (100 * 44100) / 1000;
-  long outputOffset = (70 * 44100) / 1000;
-  long overlapLength = segmentLength - outputOffset;
+  int segmentLength = (100 * 44100) / 1000;
+  int outputOffset = (70 * 44100) / 1000;
+  int overlapLength = segmentLength - outputOffset;
 
   // Calculates the output length and the number of segments needed to fill it
-  long outputLength = (long)(inputLength * multiplier);
+  int outputLength = (int)(inputLength * multiplier);
   *output = calloc(outputLength, sizeof(float));
-  long numSegments = (long)ceil((outputLength - overlapLength) / (float)outputOffset);
+  int numSegments = (int)ceil((outputLength - overlapLength) / (float)outputOffset);
   if (segmentLength > outputLength || segmentLength > inputLength || numSegments < 2) {
     return outputLength;
   }
 
   // Calculates the offset between segments in the input array based on how much stretching is needed
-  long inputOffset = (long)ceil((inputLength - segmentLength) / (float)(numSegments - 1));
+  int inputOffset = (int)ceil((inputLength - segmentLength) / (float)(numSegments - 1));
   float *outputMaxAmp = calloc(outputLength, sizeof(float));
 
   // Sets up the window function used to smoothly fade between segments
@@ -146,27 +145,27 @@ long timeStretch(float *input, long inputLength, float **output, float multiplie
 
   // Copies the first segment from the start of the input to the start of the output
   float nextSegment[segmentLength];
-  long halfSegment = segmentLength / 2;
+  int halfSegment = segmentLength / 2;
   copyArray(input, nextSegment, halfSegment);
   multiplyArrays(input + halfSegment, window + halfSegment, nextSegment + halfSegment, segmentLength - halfSegment);
   addArrays(nextSegment, *output, *output, segmentLength);
   addScalarToArray(outputMaxAmp, 1.0, halfSegment);
   addArrays(window + halfSegment, outputMaxAmp + halfSegment, outputMaxAmp + halfSegment, segmentLength - halfSegment);
 
-  long lastInputStart = 0;
-  long maxShift = (10 * 44100) / 1000;
+  int lastInputStart = 0;
+  int maxShift = (10 * 44100) / 1000;
   maxShift = maxShift < inputOffset ? maxShift : inputOffset;
 
   // Copies the middle segments from their input positions to their corresponding output positions
-  for (long i = 1; i < numSegments - 1; i++) {
-    long inputStart = inputOffset * i;
-    long outputStart = outputOffset * i;
+  for (int i = 1; i < numSegments - 1; i++) {
+    int inputStart = inputOffset * i;
+    int outputStart = outputOffset * i;
 
     // Finds the value within maxShift that aligns the next segment best with the previous one to avoid phase cancellation
     float bestSum = 0;
-    long bestShift = 0;
-    long shiftHigh = inputStart + segmentLength + maxShift < inputLength ? maxShift : inputLength - segmentLength - inputStart;
-    for (long j = -maxShift; j < shiftHigh; j += 4) {
+    int bestShift = 0;
+    int shiftHigh = inputStart + segmentLength + maxShift < inputLength ? maxShift : inputLength - segmentLength - inputStart;
+    for (int j = -maxShift; j < shiftHigh; j += 4) {
       float sum = dotProduct(input + inputStart + j, input + lastInputStart + outputOffset, overlapLength);
       if (j == -maxShift || sum > bestSum) {
         bestSum = sum;
@@ -183,8 +182,8 @@ long timeStretch(float *input, long inputLength, float **output, float multiplie
   }
 
   // Copies the last segment from the end of the input to the end of the output
-  long inputStart = inputLength - segmentLength;
-  long outputStart = outputLength - segmentLength;
+  int inputStart = inputLength - segmentLength;
+  int outputStart = outputLength - segmentLength;
   multiplyArrays(input + inputStart, window, nextSegment, halfSegment);
   copyArray(input + inputStart + halfSegment, nextSegment + halfSegment, segmentLength - halfSegment);
   addArrays(nextSegment, *output + outputStart, *output + outputStart, segmentLength);
@@ -192,7 +191,7 @@ long timeStretch(float *input, long inputLength, float **output, float multiplie
   addScalarToArray(outputMaxAmp + outputStart + halfSegment, 1.0, segmentLength - halfSegment);
 
   // Normalizes the output array
-  for (long i = 0; i < outputLength; i++) {
+  for (int i = 0; i < outputLength; i++) {
     float quotient = outputMaxAmp[i] > 0 ? (*output)[i] / outputMaxAmp[i] : (*output)[i];
     quotient = quotient <= 1.0 ? quotient : 1.0;
     quotient = quotient >= -1.0 ? quotient : -1.0;
@@ -212,14 +211,14 @@ long timeStretch(float *input, long inputLength, float **output, float multiplie
  * stretch -- multiplier for the audio's duration
  * semitones -- number of semitones to shift pitch
  */
-struct result *repitchAndStretch(float *data, long length, float stretch, int semitones) {
+struct result *repitchAndStretch(float *data, int length, float stretch, int semitones) {
   float multiplier = pow(1.05946, semitones);
 
   float *stretched;
-  long stretchedLength = timeStretch(data, length, &stretched, stretch * multiplier);
+  int stretchedLength = timeStretch(data, length, &stretched, stretch * multiplier);
 
   float *resampled;
-  long resampledLength = resample(stretched, stretchedLength, &resampled, multiplier);
+  int resampledLength = resample(stretched, stretchedLength, &resampled, multiplier);
   free(stretched);
 
   struct result *output = malloc(sizeof(struct result));
@@ -235,7 +234,7 @@ struct result *repitchAndStretch(float *data, long length, float stretch, int se
  *
  * length -- number of audio samples in input
  */
-float *allocateInputMemory(long length) {
+float *allocateInputMemory(int length) {
   return calloc(length, sizeof(float));
 }
 
