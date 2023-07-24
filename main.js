827348eff426200b2fa956f5246e1dd0bd15fa09
Amogh Shivaram <ashivaram@wisc.edu>
2023-07-24 13:10:59 -0500

    more work on js

--- main.js
+++ main.js
@@ -1,88 +1,101 @@
+// Check that browser supports key features here and add div to dom with message if not
+
+const audioContext = new AudioContext();
+let originalAudioBuffer;
+let activeAudioBuffer;
+let bufferSource;
+
 const memory = new WebAssembly.Memory({initial: 256, maximum: 32768});
 const importObject = {env: {memory: memory, emscripten_notify_memory_growth: growMemory}};
 const wasmSource = WebAssembly.instantiateStreaming(fetch("repitch.wasm"), importObject);
-
-const audioContext = new AudioContext();
-const playback = document.getElementById("playback");
-let originalAudioBuffer;
 
 const fileChooser = document.getElementById("file-select");
 fileChooser.addEventListener("change", openFile);
 document.getElementById("file-button").onclick = (e) => fileChooser.click();
 
-function openFile(event) {
-  const file = fileChooser.files[0];
-  const filenameLabel = document.getElementById("filename");
-  filenameLabel.innerText = file.name;
-  playback.src = URL.createObjectURL(file);
-
-  const reader = new FileReader();
-  reader.onload = (e) => {
-    audioContext.decodeAudioData(e.target.result).then((audioBuffer) => {
-      originalAudioBuffer = audioBuffer;
-      processAudioBuffer(audioBuffer);
-    });
-  };
-  reader.readAsArrayBuffer(file);
-
-  // Maybe deal with allocating the input memory for wasm module here (and only free it with another method when new file opened), so same thing can be accessed
-}
-
-async function processAudioBuffer(audioBuffer) {
-  // Do both channels properly and then send back to playback src (or change playback to be custom audiocontext play instead of basic html audio), make downloadable, etc
-  const leftChannel = audioBuffer.getChannelData(0);
-  const rightChannel = audioBuffer.getChannelData(1);
-
-  await wasmSource.then(source => {
-    let inputPointer = source.instance.exports.allocateInputMemory(leftChannel.length);
-    console.log(inputPointer);
-    
-    new Float32Array(memory.buffer).set(leftChannel, inputPointer / 4);
-    let outputPointer = source.instance.exports.repitchAndStretch(inputPointer, leftChannel.length, timeSlider.value, pitchSlider.value);
-    console.log(outputPointer);
-    let outputLength = new Int32Array(memory.buffer)[outputPointer / 4];
-    let outputDataPointer = new Int32Array(memory.buffer)[1 + (outputPointer / 4)];
-    console.log(outputLength);
-    console.log(outputDataPointer);
-
-    console.log(new Float32Array(memory.buffer)[outputDataPointer / 4]);
-
-    const outputDataInMemory = new Float32Array(memory.buffer, outputDataPointer, outputLength);
-    const outputDataCopy = new ArrayBuffer(outputLength * 4);
-    new Float32Array(outputDataCopy).set(outputDataInMemory);
-
-    source.instance.exports.freeAllocatedMemory(outputPointer); // Comment this out to cause the memory growth errors in console and fix them independently, since those should be prevented regardless, then add this back in
-
-    const bufferSource = audioContext.createBufferSource();
-    let newAudioBuf = audioContext.createBuffer(1, outputLength, 44100);
-    newAudioBuf.copyFromChannel(new Float32Array(outputDataCopy), 0, 0);
-    bufferSource.buffer = newAudioBuf;
-    bufferSource.connect(audioContext.destination);
-    bufferSource.start(audioContext.currentTime);
-    console.log(newAudioBuf);
-  });
-}
+const playButton = document.getElementById("play");
+playButton.addEventListener("click", playAudio);
 
 const pitchSlider = document.getElementById("semitones");
 const pitchLabel = document.getElementById("semitones-label");
 pitchSlider.addEventListener("input", (e) => pitchLabel.innerText = pitchSlider.value);
-pitchSlider.addEventListener("change", updateAudio);
+pitchSlider.addEventListener("change", processAudioBuffer);
 
 const timeSlider = document.getElementById("time-stretch");
 const timeLabel = document.getElementById("time-stretch-label");
 const timeFormat = new Intl.NumberFormat("en-US", {minimumFractionDigits: 2, maximumFractionDigits: 2});
 timeSlider.addEventListener("input", (e) => timeLabel.innerText = timeFormat.format(timeSlider.value));
-timeSlider.addEventListener("change", updateAudio);
+timeSlider.addEventListener("change", processAudioBuffer);
 
-function updateAudio() {
-  // DONT do this. Buffer will get "detached" at least on firefox, instead store either before step or after step's _
-  if (originalAudioArrayBuffer != undefined) {
-    audioContext.decodeAudioData(originalAudioArrayBuffer).then((audioBuffer) => processAudioBuffer(audioBuffer));
-    console.log(`${pitchSlider.value}, ${timeSlider.value}`);
+function openFile(event) {
+  const file = fileChooser.files[0];
+  const filenameLabel = document.getElementById("filename");
+  filenameLabel.innerText = file.name;
+
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    audioContext.decodeAudioData(e.target.result).then((audioBuffer) => {
+      originalAudioBuffer = audioBuffer;
+      activeAudioBuffer = audioBuffer;
+      processAudioBuffer();
+    });
+  };
+  reader.readAsArrayBuffer(file);
+}
+
+function playAudio(event) {
+  if (bufferSource != undefined) {
+    bufferSource.disconnect(audioContext.destination);
   }
+  bufferSource = audioContext.createBufferSource();
+  bufferSource.buffer = activeAudioBuffer;
+  bufferSource.connect(audioContext.destination);
+  bufferSource.start();
+}
+
+async function processAudioBuffer() {
+  if (originalAudioBuffer == undefined) {
+    return;
+  }
+
+  await wasmSource.then(source => {
+    console.log("Loading..."); // Gray stuff out (like download button and playback bar) while this is processing
+    let newAudioBuffer;
+    
+    for (let i = 0; i < originalAudioBuffer.numberOfChannels; i++) {
+      const channelData = originalAudioBuffer.getChannelData(i);
+      let inputPointer = source.instance.exports.allocateInputMemory(channelData.length);
+      new Float32Array(memory.buffer).set(channelData, inputPointer / 4);
+
+      let outputPointer = source.instance.exports.repitchAndStretch(inputPointer, channelData.length, timeSlider.value, pitchSlider.value);
+      let outputLength = new Int32Array(memory.buffer)[outputPointer / 4];
+      if (newAudioBuffer == undefined) {
+        newAudioBuffer = audioContext.createBuffer(originalAudioBuffer.numberOfChannels, outputLength, 44100);
+      }
+      let outputDataPointer = new Int32Array(memory.buffer)[1 + (outputPointer / 4)];
+
+      const outputDataInMemory = new Float32Array(memory.buffer, outputDataPointer, outputLength);
+      const outputDataCopy = new ArrayBuffer(outputLength * 4);
+      new Float32Array(outputDataCopy).set(outputDataInMemory);
+
+      source.instance.exports.freeAllocatedMemory(outputPointer);
+      newAudioBuffer.copyToChannel(new Float32Array(outputDataCopy), i, 0);
+    } 
+    
+    activeAudioBuffer = newAudioBuffer;
+    console.log("Done");
+  });
 }
 
 function growMemory() {
-  memoryFloat32View = new Float32Array(memory.buffer);
+  console.log("memory grow notify");
   // Handle memory growth
 }
+
+// Next step: make it faster. It works, but when eg try mp3 thats 2 mins long, it takes a second to process, which makes sense but make it faster and TEST it to make sure can see the fastness
+// Especially when first upload (and it doesnt even put filename in the thing until prints Done to console for the wasm part. Why?)
+// This means both on the JS and C side. On C side there's a few things can try out, but mainly on JS side be more efficient like copying arrays or like pre allocating input memory etc, or something
+
+// Not good-- on firefox it lags when eg 1 minute audio file like if click high piutch, while the wasm is running, which takes too long (a second, not instant), if they try to change the slider, then that doesnt happen until the thing ends, then it does that late and lags further, and so on.
+  // It should at least block user action and visibly show that it's working on it, and/or not process slider changes while still doing the last thing so can still ffeel responsive
+  // but better to also make it faster in the first place and ALSO either block action or nor run it if something still going on.
